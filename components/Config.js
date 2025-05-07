7f8c49089e0c4713155bff5c401177b12a1ac039
Sergio Ramos <sergioramosdavid@hotmail.com>
2025-05-07 13:24:32 -0400

    config cambiado

--- components/Config.js
+++ components/Config.js
@@ -1,4 +1,5 @@
-export const API_URL = "http://localhost:3001";
+// export const API_URL = "http://localhost:3001";
+export const API_URL = "http://167.99.150.94:3001";
 
 export const mapboxglAccessToken =
   "pk.eyJ1Ijoic2VyZ2lvZGF2aWRyYW1vcyIsImEiOiJjbThxc3E3MW0wYm1wMmpweXd3emZpYm4yIn0.PHjsYpeMzmWdDGitl8hp-g";
