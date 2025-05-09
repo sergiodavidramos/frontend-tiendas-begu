b0c681434194263653fc98b70659da90a0cc0b1b
Sergio Ramos <sergioramosdavid@hotmail.com>
2025-05-09 01:12:55 -0400

    cambiando la apiURL

--- components/Config.js
+++ components/Config.js
@@ -1,5 +1,5 @@
 // export const API_URL = "http://localhost:3001";
-export const API_URL = "http://167.99.150.94:3001";
+export const API_URL = "https://api-begu.fribar.bo";
 
 export const mapboxglAccessToken =
   "pk.eyJ1Ijoic2VyZ2lvZGF2aWRyYW1vcyIsImEiOiJjbThxc3E3MW0wYm1wMmpweXd3emZpYm4yIn0.PHjsYpeMzmWdDGitl8hp-g";
